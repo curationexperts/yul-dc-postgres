5f20b3e30e1fef9ac8e4b7d93180567ef872669b
McClain Looney <m@loonsoft.com>
2020-11-04 14:24:53 -0600

    poc

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,7 @@
 FROM postgres:12.3
 
+ARG DYNATRACE_TOKEN
+RUN apt-get update && apt install curl -y && apt clean
 COPY docker-entrypoint-initdb.d /docker-entrypoint-initdb.d
+COPY ops/boot.sh /boot.sh
+RUN chmod 755 /boot.sh
---
+++ ops/boot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -ex
+if [ ! -z "$DYNATRACE_TOKEN" ];then
+  curl -Ls -H "Authorization: Api-Token ${DYNATRACE_TOKEN}" 'https://nhd42358.live.dynatrace.com/api/v1/deployment/installer/agent/unix/default/latest?arch=x86&flavor=default' > installer.sh
+
+  /bin/sh installer.sh --set-app-log-content-access=true --set-infra-only=true --set-host-group=DC --set-host-name=${CLUSTER_NAME}-worker 2>&1 & 
+fi
+su postgres -c 'exec /docker-entrypoint.sh  postgres'
